65228b71c289e1c05b6467a45e92a0e7e68b2ec2
Mangell06 <mangell0624@gmail.com>
2025-10-27 17:27:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,43 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+     <noscript>
+        <div class="maincontainer">
+        <form action="./play.php" method="post" class="datacontainer">
+            <h1>CipherType</h1>
+            <div class="incontainer">
+                <input type="text" id="inname" name="inname" placeholder="Introduzca su nombre">
+                <select name="indifficulty" id="indifficulty">
+                    <option value="sencillo">Sencillo</option>
+                    <option value="normal">Normal</option>
+                    <option value="experto">Experto</option>
+                </select>
+                <button type="submit" id="buttonInitialitze" disabled>Inicializar</button>
+            </div>
+        </form>
+        <div class="datacontainer">
+            <h1>Description</h1>
+            <p>
+                Mi querido Watson, este juego es una batalla entre tu mente
+                y el paso del tiempo: las palabras son las pistas, y tu rapidez 
+                y precisión al escribir son la clave para ganar.
+            </p>
+        </div>
+    </div>
+    <img class="mesa" src="media/mesamesa.jpg" alt="Imagen de una mesa">
+    <div class="machine">
+        <img src="media/typingmachine.png" alt="Imagen de máquina de escribir">
+    </div>
+    </noscript>
     <div class="maincontainer">
         <form action="./play.php" method="post" class="datacontainer">
             <h1>CipherType</h1>
             <div class="incontainer">
                 <input type="text" id="inname" name="inname" placeholder="Introduzca su nombre">
                 <select name="indifficulty" id="indifficulty">
-                    <option value="Sencillo">Sencillo</option>
-                    <option value="Normal">Normal</option>
-                    <option value="Experto">Experto</option>
+                    <option value="sencillo">sencillo</option>
+                    <option value="normal">normal</option>
+                    <option value="experto">experto</option>
                 </select>
                 <button type="submit" id="buttonInitialitze">Inicializar</button>
             </div>
@@ -30,9 +58,9 @@
             </p>
         </div>
     </div>
-    <img class="mesa" src="media/mesamesa.jpg" alt="">
+    <img class="mesa" src="media/mesamesa.jpg" alt="Imagen de una mesa">
     <div class="machine">
-        <img src="media/typingmachine.png" alt="">
+        <img src="media/typingmachine.png" alt="Imagen de máquina de escribir">
     </div>
 </body>
 <script>
