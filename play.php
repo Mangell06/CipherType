cd42936b043718f0631241578ffc1eb9321df731
xhector7 <loopsis753@gmail.com>
2025-10-27 18:42:16 +0100

    Commit necesario para hacer merge de pre a mi rama por bloqueo

--- play.php
+++ play.php
@@ -10,6 +10,27 @@
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="/media/lupa.ico">
+   <!-- 
+    <style>
+        .highlight {
+            color: #ffeb3b;
+            text-decoration: underline;
+            font-weight: bold;
+            text-shadow: 0 0 10px #ffeb3b;
+            animation: glow 2s ease-in-out infinite;
+        }
+
+        @keyframes glow {
+            0%, 100% {
+                text-shadow: 0 0 5px #fff, 0 0 10px #ffeb3b;
+            }
+
+            50% {
+                text-shadow: 0 0 50px #ffeb3b, 0 0 35px #ffeb3b;
+            }
+        }
+    </style>
+-->
 </head>
 
 <body class="play">
@@ -86,6 +107,31 @@
                 div.appendChild(span);
             }
         }
+
+        /*
+        const showPhrase = () => {
+            div.innerHTML = "";
+            [...frase].forEach((char, i) => {
+                const span = document.createElement("span");
+                if (i === index) span.classList.add("highlight");
+                span.textContent = char;
+                div.appendChild(span);
+            });
+        };
+        
+        let index = 0;
+
+        document.addEventListener("keydown", e => {
+            if (e.key === frase[index]) {
+                index++;
+                if (index >= frase.length) {
+                    index = 0;
+                }
+                showPhrase();
+            }
+        });
+        showPhrase();
+        */
     </script>
     <?php
     ?>
