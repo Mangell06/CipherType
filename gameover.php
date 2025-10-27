63377334c86c6dfdd511763e4547e1c1fbacd17c
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-27 19:11:53 +0100

    cambiar diferentes cosas de gameover.php y los estilos

--- gameover.php
+++ gameover.php
@@ -6,17 +6,13 @@
     <title>Gameover</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 <body class="gameover">
     <div class="gameoverDiv">
         <h1>¿Quieres registrar tu récord?</h1>
-        <!-- tabla con los sesions -->
          <?php
-            // $name = $_SESSION['name'];
-            // $points = $_SESSION['points'];
-            $name = "Watson";
-            $points = "1500";
+            $name = $_SESSION['name'];
+            $points = $_SESSION['points'];
             echo "<table>";
             echo "<tr><th>Nombre</th><th>Puntos</th></tr>";
             echo "<tr><td>".$name."</td><td>".$points."</td></tr>";
