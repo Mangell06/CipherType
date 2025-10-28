55e44b5c025c071a22f9c91c0437daa9c4f33329
xhector7 <loopsis753@gmail.com>
2025-10-28 16:44:21 +0100

    Especificacion de marcar la letra por picar completada, falta por testear

--- play.php
+++ play.php
@@ -107,6 +107,8 @@ session_start();
         }
         ?>";
 
+        const showPhrase = () => { const span = document.getElementById("letter"+indexLetter); span.className = "highlight"; };
+
          const render = () => {
             div.innerText = "";
             for (let i = 0; i < frase.length; i++) {
@@ -115,9 +117,12 @@ session_start();
                 span.textContent = frase[i];
                 div.appendChild(span);
             }
+            showPhrase();
         }
 
         let indexLetter = 0;
+
+        
 
         function checkInput(isMayus, inletter) {
             const letter = document.getElementById("letter"+indexLetter);
@@ -132,6 +137,7 @@ session_start();
             } else {
                 points += 100;
             }
+
         }
 
         function endGame() {
@@ -144,6 +150,9 @@ session_start();
                 iscorrect = checkInput(iscorrect, e.key);
                 isCorrectLetter(iscorrect, e.key === " " ? true : false);
                 indexLetter++;
+                if (indexLetter < frase.length) {
+                    showPhrase();  
+                }
                 if (indexLetter >= frase.length) {
                     endGame();
                 }
