e7275a6b0406b0cf00703a566f4d1fc689ea9223
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-29 00:04:08 +0100

    cambios en los ficheros error y entrada de gameover

--- errors/error403.php
+++ errors/error403.php
@@ -5,24 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error 403</title>
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
-    <link rel="icon" href="/media/lupa.ico">
+    <link rel="stylesheet" href="../styles.css?no-cache=<?php echo time(); ?>">
+    <link rel="icon" href="../media/lupa.ico">
 </head>
 <body class="error">
     <div class="postit">
-        <img class="topPin" src="/media/pin.png" alt="Imagen de una chincheta">
-        <img src="/media/postit.png" alt="Imagen de una post-it">
+        <img class="topPin" src="../media/pin.png" alt="Imagen de una chincheta">
+        <img src="../media/postit.png" alt="Imagen de una post-it">
         <p>Watson, ¿cómo esperas descubrir al culpable si ni siquiera has revisado las pistas? Acceso denegado.</p>
     </div>
     <div class="smallPostits">
         <div>
-            <img class="pins" src="/media/pin.png" alt="Imagen de una chincheta">
+            <img class="pins" src="../media/pin.png" alt="Imagen de una chincheta">
             <input type="button" value="Página inicial" onclick="changePage()">
         </div>
      </div>
      <script>
         function changePage(){
-            window.location = "/play.php";
+            window.location = "/CipherType/play.php";
         }
      </script>
 </body>
