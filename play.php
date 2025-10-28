76497cad63bba504f4f266d9fb7aa1e75595d6f0
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-28 20:00:46 +0100

    sistema anticacheo css

--- play.php
+++ play.php
@@ -10,7 +10,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <link rel="icon" href="media/lupa.ico">
 </head>
 
