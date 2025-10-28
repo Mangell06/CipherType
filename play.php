dd0718ffb3a126ca80878a73485a1395f87e3728
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-28 15:39:41 +0100

    easter egg

--- play.php
+++ play.php
@@ -7,13 +7,15 @@
     <title>Play</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="/media/lupa.ico">
 </head>
 
 <body class="play">
-    <img class="mesa" src="media/mesa.jpg" alt="Imagen de una mesa">
+    <img class="mesa" src="./media/mesa.jpg" alt="Imagen de una mesa">
     <div class="machine">
         <div class="textos">
             <p id="timer"></p>
@@ -22,12 +24,68 @@
             </div>
         </div>
         <img src="media/typingmachine.png" alt="Imagen de máquina de escribir">
-    </div>
 
+    </div>
+    <img id="lupa" src="media/lupa_easteregg.png" alt="lupa easteregg">
+    <img id="vela" src="media/velaEasterEgg.png" alt="vela Easter Egg">
+    <img id="libro" src="media/libroEasterEgg.png" alt="libro Easter Egg">
+    <img id="sombrero" src="media/sombreroSherlock.png" alt="sombrero Easter Egg">
+    <img id="sherlock" class="invisible" src="media/sherlockHolmes.png" alt="sherlock">
+    <div class="invisible listaEntera">
+        <p>Lupa</p>
+        <p>Vela</p>
+        <p>Libro</p>
+        <p>Sombrero</p>
+
+    </div>
     <script>
         const p = document.getElementById("timer");
         const pInformation = document.getElementById("textStartInformation");
         const div = document.querySelector("div.text");
+        const listaDiv = document.querySelector("div.invisible");
+        const listaP = listaDiv.querySelectorAll("p");
+        const imgSherlock = document.getElementById("sherlock");
+        const ids = ["lupa", "vela", "libro", "sombrero"];
+        const nombres = ["Lupa", "Vela", "Libro", "Sombrero"];
+        win = false;
+        let eventCont = 4;
+        ids.forEach((id, index) => {
+            const element = document.getElementById(id);
+            element.addEventListener("click", () => {
+                element.classList.add("invisible");
+                alert(`Has clicado el objeto ${nombres[index]}`);
+                eventCont--;
+                if (eventCont <= 3) {
+                    listaDiv.classList.remove("invisible");
+
+                }
+                if (eventCont <= 0) {
+                    imgSherlock.classList.remove("invisible");
+                    win = true;
+                }
+
+                listaP.forEach(pItem => {
+                    if (pItem.textContent.toLowerCase() === nombres[index].toLowerCase()) {
+                        pItem.classList.remove("invisible");
+                        pItem.classList.add("found");
+                    }
+                });
+                if (win) {
+                    setTimeout(() => {
+                        alert("Gracias Watson por encontrar todos mis objetos, te obsequio con 7000 puntos más.");
+                    }, 1000);
+                    //hay que insertar que se sumen los puntos
+                    const puntos = "<?php
+                        session_start();
+                        $_SESSION['poinst'] += 7000;
+                    ?>";
+                }
+            });
+        });
+
+
+
+
         let cont = 4;
         const interval = setInterval(() => {
             if (cont <= 0) {
@@ -58,17 +116,17 @@
             array_push($sentencesLines, fgets($sentencesFile));
         }
         fclose($sentencesFile);
-
+        
         $textoSencilloSubstringTrim = trim(substr($sentencesLines[0], 9));
         $separateSentences = explode("*", $textoSencilloSubstringTrim);
-
+        
         function getRandomPhrase($stringFrases){
             $textoSubstringTrim = trim($stringFrases);
             $array = explode("*", $textoSubstringTrim);
             $randomPhraseKey = array_rand($array, 1);
             return $array[$randomPhraseKey];
         }
-
+        
         if (isset($difficulty) && $difficulty === "sencillo") {
             echo getRandomPhrase(substr($sentencesLines[0], 9));
         } else if (isset($difficulty) && $difficulty === "normal") {
@@ -86,9 +144,10 @@
                 div.appendChild(span);
             }
         }
+
+    
     </script>
-    <?php
-    ?>
+
 </body>
 
 </html>
