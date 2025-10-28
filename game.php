353eeff8f72544b48131f328ea3d8b7b769ec9a3
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-28 19:58:38 +0100

    poner lupas

--- game.php
+++ game.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game</title>
-    <link rel="icon" href="/media/lupa.ico">
+    <link rel="icon" href="media/lupa.ico">
 </head>
 <body>
     <h1 class="js-required">Tienes js activado</h1>
--- play.php
+++ play.php
@@ -11,7 +11,7 @@
         href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="/media/lupa.ico">
+    <link rel="icon" href="media/lupa.ico">
 </head>
 
 <body class="play">
