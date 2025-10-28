96082187d6495255094c5dd2eceedf68bcdc7fa5
Mangell06 <mangell0624@gmail.com>
2025-10-28 18:00:58 +0100

    parche

--- play.php
+++ play.php
@@ -41,6 +41,9 @@ session_start();
         <p>Sombrero</p>
 
     </div>
+    <form id="endForm" action="gameover.php" method="POST" style="display:none;">
+        <input type="hidden" name="points" id="pointsField">
+    </form>
     <script>
         const p = document.getElementById("timer");
         const pInformation = document.getElementById("textStartInformation");
@@ -172,20 +175,9 @@ session_start();
         }
 
         function endGame() {
-            const form = document.createElement("form");
-            form.method = "POST";
-            form.action = "gameover.php";
-
-            const input = document.createElement("input");
-            input.type = "hidden";
-            input.name = "points";
-            input.value = points;
-
-            form.appendChild(input);
-            document.body.appendChild(form);
-            form.submit();
+            document.getElementById("pointsField").value = points;
+            document.getElementById("endForm").submit();
         }
-
 
         document.addEventListener('keyup',(e) => {
             if (/^[A-Za-z ,]$/.test(e.key)) {
