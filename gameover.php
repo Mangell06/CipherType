7fdf1ecc5d38fe4b8bc22b7cfbb39a50a6858541
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-29 00:37:08 +0100

    errors y gameover en pre

--- gameover.php
+++ gameover.php
@@ -17,8 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game Over</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" href="/media/lupa.ico">
+    <link rel="stylesheet" href="styles.css?no-cache=<?php echo time(); ?>">
+    <link rel="icon" href="media/lupa.ico">
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Oswald:wght@200..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 <body class="gameover">
