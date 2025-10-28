38ef7bef3b92bbdcec12eac9768d134775db3a25
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2025-10-28 15:56:14 +0100

    game.php creado con noscript

---
+++ game.php
@@ -0,0 +1,4 @@
+<noscript>
+            <p class="error">Querido Watson, debes activar el javascript para seguirle la pista</p>
+            <button type="submit" disabled>Inicializar</
+</noscript>
