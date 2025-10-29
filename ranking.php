be8cb6c9c7fea16a1c7eecb273c08a7bf8e86388
Miguel Ángel López <mangell0624@gmail.com>
2025-10-29 20:13:41 +0100

    Fix ranking
    Show several players with the same name. Before, other players weren’t shown if their name matched yours; now they are.

--- ranking.php
+++ ranking.php
@@ -31,27 +31,27 @@
     if ($contenido !== false && !empty($contenido)) {
         $usuarios = explode('#', $contenido); // separar usuarios
         $ranking = [];
-
+        $count = 0;
         foreach ($usuarios as $usuario) {
             if (!empty($usuario) && strpos($usuario, ':') !== false) {
                 list($name, $points) = explode(':', $usuario);
-                $ranking[$name] = (int)$points;
+                $ranking[$count] = [$name,(int)$points];
+                $count ++;
             }
         }
 
-        arsort($ranking); // ordenar de mayor a menor
-        $count = 0;
+        usort($ranking, fn($nombre, $puntos) => $puntos[1] <=> $nombre[1]); // ordenar array por puntos de mayor a menor
+
         echo "<table>";
         echo "<tr><th>Nombre</th><th>Puntos</th></tr>";
-        foreach ($ranking as $name => $points) {
-            if ($_SESSION['name'] === $name && $_SESSION['points'] == $points) {
-              echo "<tr><td class='winner'>".$name."</td><td class='winner'>".$points."</td></tr>";
+        foreach ($ranking as $count => [$name,$points]) {
+            if (isset($_SESSION['name']) && isset($_SESSION['points']) && $_SESSION['name'] === $name && $_SESSION['points'] == $points) {
+                echo "<tr><td class='winner'>".$name."</td><td class='winner'>".$points."</td></tr>";
             } else if ($count % 2 === 0 && $count !== 1) {
                 echo "<tr><td class='second'>".$name."</td><td class='second'>".$points."</td></tr>";
             } else {
                 echo "<tr><td>".$name."</td><td>".$points."</td></tr>";
             }
-            $count ++;
         }
         echo "</table>";
     } else {
