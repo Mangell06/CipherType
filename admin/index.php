226ea004a64346e445e0b879e56a876852efb871
Mangell06 <mangell0624@gmail.com>
2025-11-04 18:03:17 +0100

    parche

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,7 @@
             if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
                 $username = $_SESSION['username'];
                 echo '<form action="logout.php" method="post" id="logoutcontainer">';
-                echo "<p>$username</p>";
+                echo "<p>Benvingut: $username</p>";
                 echo '<button type="submit" id="buttonLogout">Cerrar sesión</button>';
                 echo '</form>';
                 echo '<div class="toolscontainer">';
